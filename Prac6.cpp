54602442cf717ed72a7f8e519dff3627386179bf
Vaibhav Ganesh Deshpande <135240501+VaibhavGDeshpande@users.noreply.github.com>
2023-12-11 09:09:49 +0530

    Update Prac6.cpp

--- Prac6.cpp
+++ Prac6.cpp
@@ -1,35 +1,47 @@
-#include <graphics.h>
-#include <iostream>
-#include <cmath>
+#include<iostream>
+#include<graphics.h>
+#include<cmath>
 using namespace std;
-class Fractals{
-public:
-    void fractals(int x1, int y1, int x5, int y5, int i)
-    {
-        int segment_x=(x5-x1);
-        int segment_y=(y5-y1);
-        int x2= x1+segment_x;
-        int y2= y1+segment_y;
-        int x4= x1+2*(segment_x);
-        int y4= y1+2*(segment_y);
-        int x3=(int)(0.5*(x1+x5)+sqrt(3)*(y5-y1)/6);
-        int y3=(int)(0.5*(y1+y5)+sqrt(3)*(x5-x1)/6);
-        if(i==0){
-            line(x1,y1,x5,y5);
-        }
-        else{
-            fractals(x1,y1,x2,y2,i-1);
-            fractals(x2,y2,x3,y3,i-1);
-            fractals(x3,y3,x4,y4,i-1);
-            fractals(x4,y4,x5,y5,i-1);
-        }
-    }
+
+
+class Fractals
+{
+	public:
+		void fract(int x1,int y1,int x5,int y5,int i)
+		{
+			
+			if(i==0)
+			{
+				line(x1,y1,x5,y5);	
+			}	
+			else
+			{
+				int segx=(x5-x1)/3;
+				int segy=(y5-y1)/3;
+				int x2=x1+segx;
+				int y2=y1+segy;
+				int x4=x1+2*segx;
+				int y4=y1+2*segy;
+				int x3=(int)(0.5*(x1+x5)+sqrt(3)*(y1-y5)/6);
+				int y3=(int)(0.5*(y1+y5)+sqrt(3)*(x5-x1)/6);
+				fract(x1,y1,x2,y2,i-1);
+				fract(x2,y2,x3,y3,i-1);
+				fract(x3,y3,x4,y4,i-1);
+				fract(x4,y4,x5,y5,i-1);
+			}
+		}	
 };
-int main() {
-    int gd=DETECT,gm;
-    initgraph(&gd,&gm,NULL);
-    Fractals f;
-    f.fractals(100,100,200,200,2);
-    getch();
-    closegraph();
-    return 0; }
+int main()
+{
+	int gd=DETECT,gmode,error;
+	initgraph(&gd,&gmode,NULL);
+	Fractals f;
+	int i;
+	cin>>i;
+	f.fract(150,20,20,280,i);
+	f.fract(280,280,150,20,i);
+	f.fract(20,280,280,280,i);
+	getch();
+	closegraph();
+	
+}
